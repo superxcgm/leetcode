f65ff2715dd7daaf0d94236f2a4a6963671ba5ca
superxcgm <superxcgm@gmail.com>
2020-05-25 22:56:57 +0800

    feat: solve 21

---
+++ 21/main.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Yujia Li  on 2020/5/25.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* ret = new ListNode();
+
+        ListNode* p = ret;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+
+            p = p->next;
+        }
+        p->next = l1 ? l1: l2;
+
+        return ret->next;
+    }
+};
